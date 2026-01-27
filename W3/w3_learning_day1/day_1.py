6cac14c094a99f33b8c07d7de80892958b4b3bc1
Andrew Frans <andrew.frans@aosmobility.com>
2026-01-27 13:35:43 -0500

    Python - Modify Strings

--- W3/w3_learning_day1/day_1.py
+++ W3/w3_learning_day1/day_1.py
@@ -337,3 +337,10 @@ print(b[2:5])
 print(b[:5])
 print(b[2:])
 print(b[-5:-2])
+
+a = "Hello, World!"
+print(a.upper())
+print(a.lower())
+print(a.strip())
+print(a.replace("H", "J"))
+print(a.split("l"))
