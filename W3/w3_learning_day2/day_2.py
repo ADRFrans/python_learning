db19054576adf39f060496fbba6f455b5cf53678
Andrew Frans <andrew.frans@aosmobility.com>
2026-01-28 11:27:51 -0500

    String format, String Concatenation

--- W3/w3_learning_day2/day_2.py
+++ W3/w3_learning_day2/day_2.py
@@ -6,3 +6,25 @@ print(a.lower())
 print(a.strip())
 print(a.replace("H", "J"))
 print(a.split(","))
+
+# Lesson - Python - String Concatenation
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#Lesson - Python - String Formatting
+age = 25
+txt = f"My name is John, and I am {age}"
+print(txt)
+
+price = 59 
+txt = f"The price is {price} dollars"
+print(txt)
+
+price = 59 
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
