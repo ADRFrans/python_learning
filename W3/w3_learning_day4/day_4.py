3d24ca7cbece11424eba03df1ff594ae9b410257
Andrew Frans <darkvaderismyfav69@gmail.com>
2026-01-30 08:50:52 -0500

    Python - sort Lists

---
+++ W3/w3_learning_day4/day_4.py
@@ -0,0 +1,38 @@
+#Lesson - Python Sort Lists 
+
+#Example - Sort the list Alphabetically: 
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+#Example - sort the list numerically 
+thisnumlist = [100, 50, 65, 82, 23]
+thisnumlist.sort()
+print(thisnumlist)
+#Example - sort the list descending 
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+
+#Example - sort the list descending 
+thisnumlist.sort(reverse = True)
+print(thisnumlist)
+
+#Example  - sort the list based on how close the number is to 50: 
+def myfunc(n):
+    return abs(n - 50)
+
+thisnumlist.sort(key = myfunc)
+print(thisnumlist)
+
+#Example - Case sensitive sorting can gve an unexpected result: 
+thislist.sort()
+print(thislist)
+
+#Example - Perform a case-insensitive sort of the list: 
+thislist.sort(key = str.lower)
+print(thislist)
+
+#Example - Reverse the order of the list items: 
+thislist.reverse()
+print(thislist)
